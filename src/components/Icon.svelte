e9e4aaf902c79459bed40ee74ca2f8ae134616ef
specialdoom <bledea.bogdan97@gmail.com>
2020-07-10 12:16:23 +0300

    Update icons. Added proi-ui icon.

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,24 +1,25 @@
 <script>
     import {icons} from '../icons';
 
-    export let type = 'error';
+    export let type = 'proiUi';
+    export let scale = 20;
     export let color;
 
-    let clr = color ? color : icons[type].color;
+    let clr = color ? color : ;
 </script>
 
 <style>
-    .sd-icon{
+    .sd-icon {
         height: 20px;
         width: 20px;
     }
 </style>
 
-<div class="sd-icon" style="fill: {clr}">
+<div class="sd-icon" style="height: {scale}px; width: {scale}px;">
     <svg id="icon" data-name="{icons[type].name}" viewBox="0 0 12 12">
         <title>{icons[type].name}-icon</title>
         <g id="icon-content" data-name="content">
-            <path d="{icons[type].path}"/>
+            <path d="{icons[type].path}" style="{icons[type].style ? icons[type].style+';' : ''}{icons[type].hasStroke ? 'stroke:': 'fill:'}{icons[type].color}"/>
         </g>
     </svg>
 </div>
--- src/icons/index.js
+++ src/icons/index.js
@@ -3,5 +3,6 @@ import warning from './warning';
 import info from './info';
 import success from './success';
 import close from './close';
+import proiUi from './proi-ui'
 
-export const icons = {error, warning, info, success, close};
+export const icons = {error, warning, info, success, close, proiUi};
