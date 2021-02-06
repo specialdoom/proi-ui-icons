2728374fd52b85f078b4d2db696b2cdc653104a2
Bogdan Bledea <bledea.bogdan97@gmail.com>
2021-02-06 17:34:08 +0200

    Delete proi-ui icon. Removed id for svg and g entities

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -6,9 +6,9 @@
     export let scale = 20;
 </script>
 {#if icons[type]}
-    <svg id="icon" data-name="{icons[type].name}" viewBox="0 0 12 12" style="width: {scale}px; height: {scale}px;">
+    <svg data-name="{icons[type].name}" viewBox="0 0 12 12" style="width: {scale}px; height: {scale}px;">
         <title>{icons[type].name}-icon</title>
-        <g id="icon-content" data-name="content">
+        <g data-name="content">
             {#each icons[type].paths as path}
                 <path d="{path}" style="fill:{color}"/>
             {/each}
